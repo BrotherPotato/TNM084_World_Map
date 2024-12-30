f4c59286903c358a3d0fa939bc4e59114f8eb17d
Max Wiklundh <wiklundhmax@gmail.com>
2024-12-30 20:29:02 +0100

    added dynamic slider

--- WorldMapGen/Assets/Scripts/Map.cs
+++ WorldMapGen/Assets/Scripts/Map.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
+using System.Collections;
 using static Unity.Mathematics.math;
 using Unity.Mathematics;
+using UnityEngine.UI;
 // noise functions
 // https://docs.unity3d.com/Packages/com.unity.mathematics@1.3/api/Unity.Mathematics.noise.html
 
@@ -13,12 +15,15 @@ public class Map : MonoBehaviour
     public int pixWidth = 800;
     public int pixHeight = 800;
     Texture2D noiseTex;
-    public float scale = 1.0f;
+    float scale = 1.0f;
     public Color water = new Color(0.0f, 0.0f, 1.0f, 1.0f);
     public Color sand = new Color(0.9f, 0.8f, 0.5f, 1.0f);
     public Color grass = new Color(0.1f, 1.0f, 0.1f, 1.0f);
     public Color forest = new Color(0.4f, 1.0f, 0.4f, 1.0f);
     public Color mountains = new Color(0.6f, 0.5f, 0.4f, 1.0f);
+
+    enum TextureTypes {Map, Cellular}
+    private TextureTypes currentTexture = TextureTypes.Map;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -35,8 +40,22 @@ public class Map : MonoBehaviour
         
     }
 
+    public void UpdateScale(){
+        scale = GameObject.Find("Scale Slider").GetComponent<Slider>().value;
+;
+        Debug.Log(currentTexture);
+
+        if(currentTexture == TextureTypes.Map){
+            CreateMap();
+        }
+        if(currentTexture == TextureTypes.Cellular){
+            CellularTex();
+        }
+    }
+
     public void CreateMap()
     {
+        currentTexture = TextureTypes.Map;
         // Set up the texture and a Color array to hold pixels during processing.
         noiseTex = new Texture2D(pixWidth, pixHeight);
         Color[] pix = new Color[noiseTex.width * noiseTex.height];
@@ -44,7 +63,7 @@ public class Map : MonoBehaviour
 
         // For each pixel in the texture...
         float y = 0.0F;
-
+        scale = scale * 5;
 
         while (y < noiseTex.height)
         {
@@ -86,7 +105,8 @@ public class Map : MonoBehaviour
        
     }
 
-    public void FBMtex(){
+    public void CellularTex(){
+        currentTexture = TextureTypes.Cellular;
         // Set up the texture and a Color array to hold pixels during processing.
         noiseTex = new Texture2D(pixWidth, pixHeight);
         Color[] pix = new Color[noiseTex.width * noiseTex.height];
@@ -120,8 +140,6 @@ public class Map : MonoBehaviour
         // Copy the pixel data to the texture and load it into the GPU.
         noiseTex.SetPixels(pix);
         noiseTex.Apply();
-
-
 
         this.GetComponent<MeshRenderer>().material.mainTexture = noiseTex;
     }
