fd31d9519dd22e1b40558ab01cc79af09d9f8b6d
osarbe <31066338+osarbe@users.noreply.github.com>
2025-01-10 21:56:03 +0100

    worley klumpar

--- WorldMapGen/Assets/Scripts/test.cs
+++ WorldMapGen/Assets/Scripts/test.cs
@@ -172,12 +172,12 @@ public class test : MonoBehaviour
     public void Worley(){
         noiseTex = new Texture2D(pixWidth, pixHeight);
         Color[] pix = new Color[noiseTex.width * noiseTex.height];
-        
+        float randomorg = UnityEngine.Random.Range(0, 100);
 
         // Strö ut worley points
-        int numPoints = 100; // kinda hårdkodat till 100 just nu
-        WorleyPoint[] points = new WorleyPoint[numPoints];
-        float increment = 1f / sqrt(numPoints);
+        int pointsPerRow = 10; // antalet punkter är dennas kvadrat
+        WorleyPoint[] points = new WorleyPoint[pointsPerRow * pointsPerRow];
+        float increment = 1f / (float)pointsPerRow;
         int i2 = 0;
         for (float yf = 0f; yf < 1f; yf += increment) {
             for (float xf = 0f; xf < 1f; xf += increment) {
@@ -188,34 +188,54 @@ public class test : MonoBehaviour
                 Vector3 position = new Vector3(xRand, yRand, zRand);
 
                 // Random färg
-                Color color = Color.HSVToRGB(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0.5f, 1f), 1f);
-
-                // Klumpa samman celler
-                if (UnityEngine.Random.Range(0f, 1f) < 0.75f) {
-                    if (UnityEngine.Random.Range(0f, 1f) < 0.5f){ // 50% chans att kopiera punkten ovanför
-                        if (i2 - 10 >= 0) {
-                            color = points[i2 - 10].col;
-                            position.z = points[i2 - 10].pos.z;
-                        }
-                    }
-                    else {
-                        if (i2 - 1 >= 0) { // 50% chans att kopiera punkten bredvid
-                            color = points[i2 - 1].col;
-                            position.z = points[i2 - 1].pos.z;
-                        }
-                    } 
-                }
-
-                // Gör kantpunkterna svarta, ser typ ut som hav.
-                if ((xf + increment >= 1f || xf == 0f) || (yf + increment >= 1f || yf == 0f)) { 
-                    color = new Color(0f, 0f, 0f);
-                }
-
+                Color color = Color.HSVToRGB(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 0.5f), 1f);
+                if (zRand < 0.1f) color = new Color(0f,0f,0f); // chans att vara vatten
+                
                 points[i2] = new WorleyPoint(position,color);  
                 i2++;
             }
         }
+
+        // Klumpa samman celler
+        for (int i = 0; i < points.Length; i++) {
+            int secondIndex = i;
+            if (UnityEngine.Random.Range(0f, 1f) < 1f) { // chans att klumpa med granne
+                if (UnityEngine.Random.Range(0f, 1f) < 0.5f){ // 50% chans att klumpa med punkten ovanför eller under
+                    if (UnityEngine.Random.Range(0f, 1f) < 0.5f) { // över
+                        if (i - pointsPerRow >= 0) secondIndex = i - pointsPerRow;
+                   }
+                    else { // under
+                        if (i2 + pointsPerRow < points.Length) secondIndex = i + pointsPerRow;
+                    }            
+                }
+                else { // 50% chans att klumpa med punkten höger eller vänster
+                    if (UnityEngine.Random.Range(0f, 1f) < 0.5f) { // höger
+                        if (i - 1 >= 0) secondIndex = i - 1;
+                    }
+                    else { // vänster
+                        if (i + 1 < points.Length) secondIndex = i + 1;
+                    }
+                } 
+                points[i].col = points[secondIndex].col;
+                points[i].pos.z = points[secondIndex].pos.z;
+            }
+        }
+
+        // Gör ramen svart
+        i2 = 0;
+        for (float yf = 0f; yf < 1f; yf += increment) {
+            for (float xf = 0f; xf < 1f; xf += increment) {
+
+                // Gör kantpunkterna svarta, ser typ ut som hav.
+                if ((xf + (2*increment) >= 1f || xf <= increment) || (yf + (2*increment) >= 1f || yf <= increment)) { 
+                    points[i2].col = new Color(0f, 0f, 0f);
+                    points[i2].pos.z = 0.1f;
+                }
+                i2++;
+            }
+        }
         
+        // Färglägg pixlarna
         float y = 0.0F;
         while (y < noiseTex.height)
         {
@@ -231,53 +251,57 @@ public class test : MonoBehaviour
                 Vector2 curPoint = new Vector2(xCoord, yCoord);
                 int j = 0;
                 foreach(WorleyPoint p in points){
-                    float dis = Vector2.Distance(p.pos,curPoint);
-                    distances[j] = dis * 5f;
+                    float dis = Vector2.Distance(p.pos,curPoint) * 5f; // dis är för lågt
+                    distances[j] = dis; 
                     j++;
                 }
                 Array.Sort(distances, sortedPoints);
 
-                float sample = (1f - distances[0]);
-                pix[(int)y * noiseTex.width + (int)x] = new Color(sample,sample,sample); // shade:ad med avstånd till worleypunkten
-                pix[(int)y * noiseTex.width + (int)x] *= sortedPoints[0].col; // cellerna färgas enligt worleypunktens färg
+                // Leta efter närmsta punkt som inte tillhör den aktuella klumpen.
+                j = 0;
+                for (int i = 1; i < sortedPoints.Length; i++){
+                    // Om det är samma färg. Gå vidare.
+                    if ((sortedPoints[0].col.r == sortedPoints[i].col.r &&
+                        sortedPoints[0].col.g == sortedPoints[i].col.g &&
+                        sortedPoints[0].col.b == sortedPoints[i].col.b)) {
+                        continue; 
+                    }
+                    // Om det är en ny färg eller vatten. Stanna.
+                    else {
+                        j = i;
+                        break;
+                    }
+                }
+                float val = (1f - distances[j]) * 2f;
 
+                // berg på flera oktaver
+                float sample = 0.0f;
+                float amp = sortedPoints[0].pos.z;
+                float tot = 0.0f;
+                float frq = 1.0f;
+                float2 currentPoint = float2((xCoord + randomorg) * scale, (yCoord + randomorg) * scale);
+                for (int i = 0; i < 5; i++){
+                    float noiseXY = noise.cellular(currentPoint * frq).x * noise.cellular(currentPoint * frq).y;
+                    sample += noiseXY * amp;
+                    tot += amp;
+                    amp *= 0.5f;
+                    frq *= 2.0f;
+                }
+                sample /= tot;
+                sample *= val;
+                if (sample < 0.35f) sample = 0f;
+
+                Color pixelCol = new Color(sample,sample,sample); 
+                if (sample == 0f) pixelCol = new Color(0f, 0.5f, 1f);
+
+                // För att visa klustrerna som overlays.
+                //pixelCol = (pixelCol + sortedPoints[0].col) / 2f;
+                
+                pix[(int)y * noiseTex.width + (int)x] = pixelCol;
                 x++;
             }
             y++;
         }
-
-        // high pass filter???? sänk upplösningen för att det ska inte ta en evighet
-
-        // y = 0.0F;
-        // while (y < noiseTex.height)
-        // {
-        //     float x = 0.0F;
-        //     while (x < noiseTex.width)
-        //     {
-        //         Color col = pix[(int)y * noiseTex.width + (int)x];
-        //         Color cxp1 = new Color(0f,0f,0f);
-        //         Color cxm1 = new Color(0f,0f,0f);
-        //         Color cyp1 = new Color(0f,0f,0f);
-        //         Color cym1 = new Color(0f,0f,0f);
-        //         if ((int)y * noiseTex.width + ((int)x+1) < pix.Length)
-        //             cxp1 = pix[(int)y * noiseTex.width + ((int)x+1)];
-        //         if ((int)y * noiseTex.width + ((int)x-1) > 0)
-        //             cxm1 = pix[(int)y * noiseTex.width + ((int)x-1)];
-        //         if (((int)y+1) * noiseTex.width + (int)x+1 < pix.Length)    
-        //             cyp1 = pix[((int)y+1) * noiseTex.width + (int)x+1];
-        //         if (((int)y-1) * noiseTex.width + (int)x+1 > 0)                 
-        //             cym1 = pix[((int)y-1) * noiseTex.width + (int)x+1];
-        //         col += cxp1;
-        //         col += cxm1;
-        //         col += cyp1;
-        //         col += cym1;
-        //         col /= 5f;
-        //         pix[(int)y * noiseTex.width + (int)x] -= col;
-
-        //         x++;
-        //     }
-        //     y++;
-        // }
 
         // Copy the pixel data to the texture and load it into the GPU.
         noiseTex.SetPixels(pix);
