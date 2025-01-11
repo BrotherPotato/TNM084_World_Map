73731cf45c48f4183ccbc8c4ea47fa71659c5b76
Max Wiklundh <wiklundhmax@gmail.com>
2025-01-11 14:08:05 +0100

    flat grid

--- WorldMapGen/Assets/Scripts/Grid.cs
+++ WorldMapGen/Assets/Scripts/Grid.cs
@@ -7,7 +7,7 @@ using System.Collections;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class Grid : MonoBehaviour
 {
-    public int xSize, ySize;
+    public int xSize, zSize;
 
     private Vector3[] vertices;
 
@@ -32,15 +32,15 @@ public class Grid : MonoBehaviour
 		GetComponent<MeshFilter>().mesh = mesh = new Mesh();
 		mesh.name = "Procedural Grid";
 
-		vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+		vertices = new Vector3[(xSize + 1) * (zSize + 1)];
         Vector2[] uv = new Vector2[vertices.Length];
         Vector4[] tangents = new Vector4[vertices.Length];
-		Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+		Vector4 tangent = new Vector4(0f, 1f, 0f, -1f);
 
-		for (int i = 0, y = 0; y <= ySize; y++) {
+		for (int i = 0, z = 0; z <= zSize; z++) {
 			for (int x = 0; x <= xSize; x++, i++) {
-				vertices[i] = new Vector3(x, y);
-                uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+				vertices[i] = new Vector3(x, 0, z);
+                uv[i] = new Vector2((float)x / xSize, (float)z / zSize);
                 tangents[i] = tangent;
 			}
 		}
@@ -48,8 +48,8 @@ public class Grid : MonoBehaviour
         mesh.uv = uv;
         mesh.tangents = tangents;
 
-		int[] triangles = new int[xSize * ySize * 6];
-		for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++) {
+		int[] triangles = new int[xSize * zSize * 6];
+		for (int ti = 0, vi = 0, z = 0; z < zSize; z++, vi++) {
 			for (int x = 0; x < xSize; x++, ti += 6, vi++) {
 				triangles[ti] = vi;
 				triangles[ti + 3] = triangles[ti + 2] = vi + 1;
